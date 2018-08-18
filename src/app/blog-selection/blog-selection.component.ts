4e3d7ca8395aa850989f7d75bf723d591634eceb
harleyttaeng <rebmarlin@gmail.com>
2018-08-19 00:27:42 +1000

    blog-selection component code optimization

--- src/app/blog-selection/blog-selection.component.ts
+++ src/app/blog-selection/blog-selection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Card} from '../card/card';
+import {CardService} from '../service/card.service';
 
 @Component({
   selector: 'app-blog-selection',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogSelectionComponent implements OnInit {
 
-  constructor() { }
+  cards: Card[];
+
+  constructor(private cardService: CardService) { }
 
   ngOnInit() {
+    this.getCards();
   }
+  getCards(): void {
+    this.cardService.getCards()
+      .subscribe(cards => this.cards = cards);
+}
 
 }
